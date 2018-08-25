de001bba093c2e49c81717bc48fe08e9428b72b3
Charles M. Chong <cchong.vise@gmail.com>
2018-08-24 19:43:11 -0700

    Deploy

---
+++ server/app.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+func main() {
+	// hello world
+	http.HandleFunc("/", helloworld)
+
+	// pong
+	http.HandleFunc("/api/ping", pingpong)
+
+	// get last10 posts
+	http.HandleFunc("/api/posts", getlast10posts)
+
+	// get post
+	http.HandleFunc("/api/post/", getpost)
+
+	// add post
+	http.HandleFunc("/api/post/new", addpost)
+
+	// add subscriber
+	http.HandleFunc("/api/subscribe/new", addSubscriber)
+
+	// handle image uploads
+	http.HandleFunc("/api/multiple", handleUpload)
+
+	// recaptcha confirmation
+	http.HandleFunc("/api/apply", recaptchaconfirmation)
+
+	// signup and subscribe customer
+	http.HandleFunc("/api/user/new", subscribeuser)
+
+	// listen on socket
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000" // Setting a Default port to 8000 to be used locally
+	}
+
+	http.ListenAndServe(":"+port, nil)
+
+}
+func (c *Config) Read() {
+	if _, err := toml.DecodeFile("./config/config.toml", &c); err != nil {
+		log.Fatal(err)
+	}
+}
+
+var config = Config{}
+
+func init() {
+	config.Read()
+}
