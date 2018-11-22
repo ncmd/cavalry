89ccf9ec8063472e68b9e167d40a47c2480847eb
Charles M. Chong <cchong.vise@gmail.com>
2018-11-22 00:43:17 -0800

    Deploy

--- setup.py
+++ setup.py
@@ -3,71 +3,117 @@ import subprocess
 import os
 
 """
-    The goal of this script is to automate the Dev Environment for a Windows-based Computer
-    1. Install Chocolatey
-    1. Download and install Python3 and Git Manually using Chocolatey
-    3. Clone Github Repository with Git
-    4. Run Python script
-    5. Download and Install all dev apps using Choco
-    6. 
-
+The goal: Automate the Dev Environment for a Windows Computer
 """
 
 def main():
-    # python_setup()
-    # chocolatey_setup()
-    # git_setup()
-    # nodejs_setup()
-    # react_setup()
-    # go_setup()
+    python_setup()
     github_clone()
+    chocolatey_setup()
+    git_setup()
+    nodejs_setup()
+    react_setup()
+    go_setup()
+    freefilesync_setup()
     # print("Hello World")
 
 def github_clone():
-    path = "C:\\Users\\test\\go\\test_src5"
+    path = "C:\\Users\\test\\go\\src"
+    project_path = "C:\\Users\\test\\go\\src\\cavalry"
     try:
         os.mkdir(path)
-        try:
-            os.chdir(path)
-        except  OSError:
-            pass
+        os.makedirs(path)
     except OSError:
         pass
-    
+    try:
+        os.chdir(path)
+    except  OSError:
+        pass
     try:
         subprocess.call(['git','clone','https://github.com/ncmd/cavalry.git'])
     except OSError:
         print("Git Clone failed")
+        os.chdir(project_path)
         return False
     else:
+        os.chdir(project_path)
         return True
 
 def chocolatey_setup():
     # Install Apps with Choco
-    subprocess.call(['choco','install','googlechrome','golang','heroku-cli','nodejs','yarn','vscode','sublimetext3','keepass','-y'])
-    subprocess.call(['refreshenv'])
+    try:
+        subprocess.call(['choco','install','golang','heroku-cli','nodejs',\
+            'yarn','vscode','sublimetext3','keepass',\
+            '-y'])
+        subprocess.call(['refreshenv'])
+    except OSError:
+        return False
+    else:
+        return True
+    
 
 def python_setup():
     # Checking Python Version 3.7.1 as default version
-    pythonVersion = os.popen('python --version').read()
-    if pythonVersion == 'Python 3.7.1\n':
-        print("System is using correct Python Version 3.7.1")
+    try:
+        pythonVersion = os.popen('python --version').read()
+        if pythonVersion == 'Python 3.7.1\n':
+            print("System is using correct Python Version 3.7.1")
+            # Install pytlint package  
+            subprocess.call(['python','-m','pip','install','-U','pylint',\
+                '--user'])
+            subprocess.call(['python','-m','pip','install','--upgrade','pip'])   
+    except OSError:
+        return False
+    else:
         return True
-    # Install pytlint package  
-    subprocess.call(['python','-m','pip','install','-U','pylint','--user'])
+    
 
 def git_setup():
-    subprocess.call(['git','config','--global','core.autocrlf','false'])
+    try:
+        subprocess.call(['git','config','--global','core.autocrlf','false'])
+    except OSError:
+        return False
+    else:
+        return True
+    
 
 def nodejs_setup():
-    subprocess.call(['npm','install','-g','concurrently','cross-var',\
+    try:
+        subprocess.call(['npm','install','-g','concurrently','cross-var',\
         'cross-env','firebase-tools','npm-run-all'], shell=True)
+    except OSError:
+        return False
+    else:
+        return True
 
 def react_setup():
-    subprocess.call(['npm','run','first-setup-react-all-x'], shell=True)
+    try:
+        subprocess.call(['npm','run','first-setup-react-all-x'], shell=True)
+    except OSError:
+        return False
+    else:
+        return True
+    
 
 def go_setup():
-    subprocess.call(['npm','run','first-setup-go-all-x'], shell=True)
+    try:
+        subprocess.call(['npm','run','first-setup-go-all-x'], shell=True)
+    except OSError:
+        return False
+    else:
+        return True
+
+def freefilesync_setup():
+    try:
+        subprocess.call(['C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe',
+            'Register-ScheduledTask','-Xml','(get-content \'C:\\Users\\test\\go\\src\\cavalry\Task_Scheduler_FreeFileSync.xml\' | out-string)'\
+            '-TaskName','Task_Scheduler_FreeFileSync'], shell=True)
+    except OSError:
+        return False
+    else:
+        return True
+    
+    
 
 if __name__ == '__main__':
     main()
--- test_setup.py
+++ test_setup.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
 import unittest
-from setup import github_clone
+from setup import github_clone,chocolatey_setup,python_setup,git_setup,nodejs_setup,react_setup,go_setup,freefilesync_setup
 
 class test_setup(unittest.TestCase):
-    def test_git_clone(self):
+
+    def test_github_clone(self):
         self.assertEqual(github_clone(), True)
-# def main():
-#     print(github_clone())
+    def test_chocolatey_setup(self):
+        self.assertEqual(chocolatey_setup(), True)
+    def test_python_setup(self):
+        self.assertEqual(python_setup(), True)
+    def test_git_setup(self):
+        self.assertEqual(git_setup(), True)
+    def test_nodejs_setup(self):
+        self.assertEqual(nodejs_setup(), True)
+    def test_react_setup(self):
+        self.assertEqual(react_setup(), True)
+    def test_go_setup(self):
+        self.assertEqual(go_setup(), True) 
+    def test_freefilesync_setup(self):
+        self.assertEqual(freefilesync_setup(), True)
 
 if __name__ == '__main__':
     unittest.main()
-    # main()
